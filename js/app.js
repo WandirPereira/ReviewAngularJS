f4e41dc555595e5537ad41cc10fe51a1a3011076
Wandir Pereira Filho <wandir@gmail.com>
2022-10-12 22:40:10 -0300

    ng-repeat

--- js/app.js
+++ js/app.js
@@ -1,28 +1,18 @@
-var app = angular.module('universidadApp',[]);
+(function(){
 
-app.controller(
-    'profesorCtrl', 
-    function($scope){
-        $scope.profesor = profesorData;
-        $scope.editando = {};
-
-        $scope.EditarProfesor = function(){
-                angular.copy($scope.profesor, $scope.editando );
-        }
-
-        $scope.GuardarCambios = function(){
-                angular.copy($scope.editando, $scope.profesor);
-        }
-
-        $scope.CancelarCambios = function(){
-                $scope.editando= {};
-        }
-    }
-);
-
-var profesorData = {
-	nombre: "Juan Carlos Pineda",
-	bio: "Saludos estudiante, mi nombre es Juan Carlos, encantado de conocerte, soy una apasionado instructor de matemáticas aplicadas cuánticas, más orientado a la física termonuclear. Mi vocación es ser maestro y lograr transmitir mis conocimientos a todos mis estudiantes!.",
-	edad: 47,
-	foto: "img/juancarlos.jpg"
-}
+    var app = angular.module('universidadApp',[ ]);
+    
+    app.controller('listadoCtrl', ['$scope', function($scope){
+        
+            $scope.listado = ["Fernando Herrera","Melissa Flores","Juan Carlos Pineda", "Maria Perez"];
+    
+    
+    
+    }]);
+    
+    
+    
+    
+    
+    })();
+    
